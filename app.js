330ca379b4437c78ca1e5c8894c86e82c8744ffa
sarathsureshc <sarathnandu1983@gmail.com>
2026-07-29 17:59:38 +0530

    fix(cors): add Cache-Control and Pragma to allowedHeaders

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "X-Requested-With"],
   })
 );
 
